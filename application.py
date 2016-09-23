2cc5b769d3ba83e3fd242936f0efaec9a014c1d5
Bertrand Caron <bertrand.fx.caron@gmail.com>
2016-09-24 04:56:41 +1000

    Add referees section

--- application.py
+++ application.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template, url_for, Markup
 from yaml import load
 
 app = Flask(__name__)
-config = load(open('config/config.yml').read())
+
+try:
+    config = load(open('config/config.yml').read())
+except:
+    raise Exception('ERROR: Missing config/config.yml file.')
 
 ITEM_TEMPLATE_FOR = {
     'education': 'item.html',
@@ -10,7 +14,12 @@ ITEM_TEMPLATE_FOR = {
     'publications': 'item_publication.html',
     'teaching': 'item.html',
     'awards': 'item_award.html',
+    'referees': 'item_referee.html',
 }
+
+ALL_SECTIONS = ['education', 'skills', 'publications', 'teaching', 'awards', 'referees']
+
+assert set(ALL_SECTIONS) == set(ITEM_TEMPLATE_FOR), 'ERROR: Missing item templates or sections.'
 
 def data_for_section(section_name):
     return load(open('data/{0}.yml'.format(section_name)).read())
@@ -45,7 +54,7 @@ def home():
         config=config,
         sections = [
             rendered_section(section_name)
-            for section_name in ['education', 'skills', 'publications', 'teaching', 'awards']
+            for section_name in ALL_SECTIONS
         ],
     )
 
