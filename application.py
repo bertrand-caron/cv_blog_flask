0585d8aa23c9bf0b3012f6257b85048c82bf809f
Bertrand Caron <bertrand.fx.caron@gmail.com>
2016-09-24 05:03:42 +1000

    Modularise by introducing helpers

--- application.py
+++ application.py
@@ -1,50 +1,14 @@
 from flask import Flask, render_template, url_for, Markup
 from yaml import load
 
+from helpers.sections import rendered_section, ALL_SECTIONS
+
 app = Flask(__name__)
 
 try:
     config = load(open('config/config.yml').read())
 except:
     raise Exception('ERROR: Missing config/config.yml file.')
-
-ITEM_TEMPLATE_FOR = {
-    'education': 'item.html',
-    'skills': 'item_skill.html',
-    'publications': 'item_publication.html',
-    'teaching': 'item.html',
-    'awards': 'item_award.html',
-    'referees': 'item_referee.html',
-}
-
-ALL_SECTIONS = ['education', 'skills', 'publications', 'teaching', 'awards', 'referees']
-
-assert set(ALL_SECTIONS) == set(ITEM_TEMPLATE_FOR), 'ERROR: Missing item templates or sections.'
-
-def data_for_section(section_name):
-    return load(open('data/{0}.yml'.format(section_name)).read())
-
-def rendered_data_for_section(section_name):
-    render_item = lambda item: render_template(
-        ITEM_TEMPLATE_FOR[section_name],
-        item=item,
-    )
-
-    return Markup(
-        ''.join([
-            render_item(item)
-            for item in data_for_section(section_name)
-        ])
-    )
-
-def rendered_section(section_name):
-    return Markup(
-        render_template(
-            'section.html',
-            section_name=section_name,
-            section_content=rendered_data_for_section(section_name),
-        ),
-    )
 
 @app.route('/')
 def home():
