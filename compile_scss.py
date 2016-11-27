06188bc3c106adf97f5fad45abe19fb272a47b45
Bertrand Caron <bertrand.fx.caron@gmail.com>
2016-11-27 13:51:39 +1000

    HOTFIX: Fix an encoding issue in compile_scss.py

--- compile_scss.py
+++ compile_scss.py
@@ -4,9 +4,9 @@ from sass import compile_string
 def compile_scss_file(filename: str) -> None:
     assert '.scss' in filename, filename
 
-    with open(filename.replace('.scss', ''), 'w') as fh:
+    with open(filename.replace('.scss', ''), 'wb') as fh:
         fh.write(
-            compile_string(open('static/style.css.scss').read()),
+            compile_string(open('static/style.css.scss', 'rb').read()),
         )
 
 if __name__ == '__main__':
