3640f45d9e9aad77e2b92d5bd7b5b904c73e9a79
Bertrand Caron <bertrand.fx.caron@gmail.com>
2017-12-13 09:39:14 +1000

    Feature: Cache pip on travis, get tests to fail on failure!

--- test.py
+++ test.py
@@ -1,31 +1,36 @@
 from flask import request
 import unittest
+from typing import Any
 
 from application import application, home, cv, blog, contact, data, CONFIG
 
 ROUTES = [home, cv, blog, contact, data]
 
+def validate_answer(answer: Any) -> None:
+    if answer.status != '200 OK':
+        raise AssertionError()
+
 class TestIntegration(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.app = application.test_client()
 
-    def tearDown(self):
+    def tearDown(self) -> None:
         pass
 
-    def test_home(self):
-        rv = self.app.get('/')
+    def test_home(self) -> None:
+        validate_answer(self.app.get('/'))
 
-    def test_cv(self):
-        rv = self.app.get('/cv')
+    def test_cv(self) -> None:
+        validate_answer(self.app.get('/cv'))
 
-    def test_blog(self):
-        rv = self.app.get('/blog')
+    def test_blog(self) -> None:
+        validate_answer(self.app.get('/blog'))
 
-    def test_contact(self):
-        rv = self.app.get('/contact')
+    def test_contact(self) -> None:
+        validate_answer(self.app.get('/contact'))
 
-    def test_data(self):
-        rv = self.app.get('/data')
+    def test_data(self) -> None:
+        validate_answer(self.app.get('/data'))
 
 if __name__ == '__main__':
     unittest.main()
