d9e0f8ea5a6e02ee2bbb7b342ffad619e6e421cb
Bertrand Caron <bertrand.fx.caron@gmail.com>
2018-11-29 10:02:27 +1000

    Feature: Serve static version on boto

--- tasks/upload_images_to_S3.py
+++ tasks/upload_images_to_S3.py
@@ -1,3 +1,4 @@
+from os import walk
 from os.path import basename, dirname, join, abspath, exists
 from glob import glob
 from argparse import ArgumentParser, Namespace
@@ -7,20 +8,42 @@ from boto.s3.connection import S3Connection, Location
 from boto.s3.key import Key
 from boto.s3.bucket import Bucket
 
+from application import APPLICATION
+
+BUCKET_NAME = 'bcaron'
+
+# Should match the one in your S3 bucket config
+INDEX_FILENAME = 'cv.html'
+
 IMAGE_DIR = join(dirname(dirname(abspath(__file__))), 'static', 'uploads')
 
 IMAGE_EXTENSIONS = {'.jpg', '.png', '.jpeg', '.pdf'}
 
 def upload(bucket: Bucket) -> None:
-    for image_filepath in glob(join(IMAGE_DIR, '*')):
-        if basename(image_filepath) not in all_keys:
-            print('Uploading image_filepath {0} to bucket'.format(image_filepath))
+    def upload_static_cv() -> None:
+        client = APPLICATION.test_client()
+        response = client.get('/')
+
+        with open(INDEX_FILENAME, 'wb') as fh:
+            fh.write(response.get_data())
+
+        k = Key(bucket)
+        k.key = INDEX_FILENAME
+        # Setting proper encoding (UTF-8) to cv.html
+        k.set_metadata('Content-Type', 'text/html; charset=utf-8')
+        k.set_contents_from_filename(INDEX_FILENAME)
+        k.set_acl('public-read')
+
+    upload_static_cv()
+
+    # Upload all static assets
+    for (root, subdirs, files) in walk('static'):
+        for _file in files:
             k = Key(bucket)
-            k.key = basename(image_filepath)
-            k.set_contents_from_filename(image_filepath)
+            filename = join(root, _file)
+            k.key = filename
+            k.set_contents_from_filename(filename)
             k.set_acl('public-read')
-        else:
-            print('image_filepath {0} already in bucket'.format(image_filepath))
 
 def download(bucket: Bucket) -> None:
     for k in bucket.list():
