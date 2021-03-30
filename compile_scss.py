d7ed9d0d1040dcf8d8907207c607cfbb23651aaf
Bertrand Caron <bertrand.fx.caron@gmail.com>
2021-03-31 07:58:13 +1000

    Feature: Try swapping sass (deprecated) to scss

--- compile_scss.py
+++ compile_scss.py
@@ -1,12 +1,12 @@
 from sys import argv
-from sass import compile_string
+from scss import Compiler
 
 def compile_scss_file(filename: str) -> None:
     assert '.scss' in filename, filename
 
     with open(filename.replace('.scss', ''), 'wb') as fh:
         fh.write(
-            compile_string(open('static/style.css.scss', 'rb').read()),
+            Compiler().compile_string(open('static/style.css.scss', 'rb').read()),
         )
 
 if __name__ == '__main__':
